20285ccfeb7a33c25763ef01c979205dcccf393a
DeSilvaMPD <Praveen.Desilva@syscolabs.com>
2023-02-27 16:23:05 +0530

    Update Report Part

--- src/test/k6/door-buster-sale.js
+++ src/test/k6/door-buster-sale.js
@@ -1,8 +1,9 @@
 import http from 'k6/http';
 import { sleep } from "k6";
 
-import { htmlReport } from '../dist/bundle.js'
+import { htmlReport } from '/Users/praveend/Documents/GitHub/k6/tqe-perf-eng-qPerfBaseProject-k6/dist/bundle.js'
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
+
 
 export function handleSummary(data) {
   return {
@@ -18,9 +19,9 @@ export const options = {
             startRate: 10,
             stages: [
                 // Level at 10 iters/s for 10 seconds
-                { target: 10, duration: "10s" },
+                { target: 10, duration: "100s" },
                 // Spike from 10 iters/s to 150 iters/s in 5 seconds!
-                { target: 150, duration: "5s" },
+                { target: 150, duration: "500s" },
                 // Level at 150 iters/s for 10 seconds
                 { target: 150, duration: "10s" },
                 // Slowing down from 150 iters/s to 100 iters/s over 20 seconds
@@ -28,7 +29,7 @@ export const options = {
                 // Leveled off at 30 iters/s for remainder
                 { target: 80, duration: "10s" },
             ],
-            preAllocatedVUs: 5,
+            preAllocatedVUs: 10,
             maxVUs: 50,
         },
     },
