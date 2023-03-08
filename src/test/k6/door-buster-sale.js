46ba0a38d20003bc744baecdf568b2acecc50d85
AsankaV <asanka.vithanage@syscolabs.com>
2023-03-08 19:00:20 +0530

    change the image

--- src/test/k6/door-buster-sale.js
+++ src/test/k6/door-buster-sale.js
@@ -21,15 +21,15 @@ export const options = {
             startRate: 10,
             stages: [
                 // Level at 10 iters/s for 10 second
-                { target: 10, duration: "100s" },
+                { target: 10, duration: "10s" },
                 // Spike from 10 iters/s to 150 iters/s in 5 seconds!
-                { target: 150, duration: "50s" },
+                { target: 150, duration: "10s" },
                 // Level at 150 iters/s for 10 seconds
-                { target: 150, duration: "100s" },
+                { target: 150, duration: "10s" },
                 // Slowing down from 150 iters/s to 100 iters/s over 20 seconds
-                { target: 100, duration: "200s" },
+                { target: 100, duration: "20s" },
                 // Leveled off at 30 iters/s for remainder
-                { target: 80, duration: "100s" },
+                { target: 80, duration: "10s" },
             ],
             preAllocatedVUs: 10,
             maxVUs: 50,
