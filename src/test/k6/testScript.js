9d451861284dc59f447a8f5e1ffd46d856e18b22
MalshaUdani <malsha.nagahawatta@gmail.com>
2023-03-30 15:05:21 +0530

    script updated

--- src/test/k6/testScript.js
+++ src/test/k6/testScript.js
@@ -6,6 +6,14 @@ import { sleep } from "k6";
 import { htmlReport } from '../dist/bundle.js'
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
 
+export function handleSummary(data) {
+  return {
+    //'/Users/praveend/Documents/GitHub/k6/tqe-perf-eng-qPerfBaseProject-k6/src/test/Reports/k6summary.html': htmlReport(data, { debug: false }),
+    'k6-report/k6summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
+}
+
 export const options = {
   discardResponseBodies: true,
   scenarios: {
