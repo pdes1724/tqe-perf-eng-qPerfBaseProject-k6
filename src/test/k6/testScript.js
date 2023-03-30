8c46791be70824758dc6a4a38548805c0fabec77
MalshaUdani <malsha.nagahawatta@gmail.com>
2023-03-30 14:36:17 +0530

    added new script

---
+++ src/test/k6/testScript.js
@@ -0,0 +1,37 @@
+import http from 'k6/http';
+
+import { sleep } from "k6";
+
+//import { htmlReport } from '../src/test/dist/bundle.js'
+import { htmlReport } from '../dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
+
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'constant-vus',
+      exec: 'contacts',
+      vus: 50,
+      duration: '30s',
+    },
+    news: {
+      executor: 'per-vu-iterations',
+      exec: 'news',
+      vus: 50,
+      iterations: 100,
+      startTime: '30s',
+      maxDuration: '1m',
+    },
+  },
+};
+
+export function contacts() {
+  http.get('https://test.k6.io/contacts.php', {
+    tags: { my_custom_tag: 'contacts' },
+  });
+}
+
+export function news() {
+  http.get('https://test.k6.io/news.php', { tags: { my_custom_tag: 'news' } });
+}
